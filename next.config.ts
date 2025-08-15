4da62b15ed87655f0fb84f6c7482f23f7d5acf9b
Phạm Dương <pduongpdu99@gmail.com>
2025-08-15 16:10:21 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.json$/,
+      type: "javascript/auto",
+      include: path.resolve(__dirname, "data"),
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
