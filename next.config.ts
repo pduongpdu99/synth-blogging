c00f93e9e19b0e25c32800238d1b44768e366f02
Phạm Dương <pduongpdu99@gmail.com>
2025-08-19 20:40:18 +0700

    config(dependencies): install plugin netlify for nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,19 @@
 import type { NextConfig } from "next";
-import path from "path";
+// import path from "path";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  // reactStrictMode: true,
+  // swcMinify: true,
   output: "export",
-  webpack: (config) => {
-    config.resolve.alias['@'] = './src';
-    config.module.rules.push({
-      test: /\.json$/,
-      type: "javascript/auto",
-      include: path.resolve(__dirname, "data"),
-    });
-    return config;
-  },
+  // webpack: (config) => {
+  //   config.resolve.alias['@'] = './src';
+  //   config.module.rules.push({
+  //     test: /\.json$/,
+  //     type: "javascript/auto",
+  //     include: path.resolve(__dirname, "data"),
+  //   });
+  //   return config;
+  // },
 };
 
 export default nextConfig;
